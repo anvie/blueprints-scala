59aca50e617e7ff10379fd96a4c2107da3e35b28
robin <robin@ansvia.com>
2013-04-15 01:36:25 +0700

    * Add AbstractDbObject. * Restructure code.
    Conflicts:
    	build.sbt

--- src/main/scala/com/ansvia/graph/BlueprintsWrapper.scala
+++ src/main/scala/com/ansvia/graph/BlueprintsWrapper.scala
@@ -16,7 +16,7 @@ import scala.reflect.runtime.universe._
 object BlueprintsWrapper {
     import scala.collection.JavaConversions._
 
-    sealed trait Wrapper
+
 
     case class ScalasticPropertyAccessor[A <: Element : ClassTag](var obj:A) {
 
@@ -125,52 +125,6 @@ object BlueprintsWrapper {
     implicit def edgeToPropertyAccessor(elm:Edge) = ScalasticPropertyAccessor(elm)
     implicit def elementToPropertyAccessor(elm:Element) = ScalasticPropertyAccessor(elm)
 
-    /**
-     * Edge wrapper on arrow chain.
-     * This wrapper automatic used via VertexWrapper.
-     * @param vertex vertex
-     * @param label label
-     * @param db database
-     */
-    case class EdgeWrapper(var vertex:Vertex, var label:String, db:Graph) extends Wrapper {
-        private var lastEdge:Edge = null
-        var prev:Option[VertexWrapper] = None
-
-        def -->(inV:Vertex):VertexWrapper = {
-            lastEdge = db.addEdge(null, vertex, inV, label)
-
-            // for performance reason
-            // we using previous object if any
-
-            val p = prev.getOrElse {
-                VertexWrapper(inV, label, db)
-            }
-
-            p.prev = Some(this)
-            p.vertex = inV
-            p
-        }
-
-        def <--(outV:Vertex):VertexWrapper = {
-            lastEdge = db.addEdge(null, outV, vertex, label)
-
-            // for performance reason
-            // we using previous object if any
-
-            val p = prev.getOrElse {
-                VertexWrapper(outV, label, db)
-            }
-            p.prev = Some(this)
-            p.vertex = outV
-            p
-        }
-
-        def -->(o:DbObject):VertexWrapper = {
-            this.-->(o.getVertex)
-        }
-
-        def <():Edge = this.lastEdge
-    }
 
     /**
      * Vertex wrapper on arrow chain.
@@ -485,7 +439,7 @@ object BlueprintsWrapper {
         }
     }
 
-    trait DbObject {
+    trait DbObject extends AbstractDbObject {
 
         protected var vertex:Vertex = null
 
@@ -548,7 +502,7 @@ object BlueprintsWrapper {
          * @param label edge label.
          * @return
          */
-        def -->(label:String)(implicit db:Graph) = {
+        def -->(label:String)(implicit db:Graph):EdgeWrapper = {
             vertex --> label
         }
 
